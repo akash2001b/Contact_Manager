f12bd8e35effde92123e211ceaf493e07a01acc2
akash Roy Choudhury <arc.ar602@gmail.com>
2021-06-13 22:21:22 +0530

    Backend Server completed

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express=require('express');
 const morgan = require("morgan");
+const connectDB = require('./config/db');
+const conectDB=require('./config/db') 
 
 
 const app=express();
+
+// connect to database
+connectDB();
+
+// init middleware
+app.use(express.json({ extended: false }));
 
 const PORT=process.env.PORT || 5000;
 
@@ -16,5 +24,7 @@ app.get('/', (req,res)=>{
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
+
+
 
 app.listen(PORT, ()=> console.log(`Server started on port ${PORT}`));
