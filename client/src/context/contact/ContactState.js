f75de8c8e5766aa850307c6d91455b32b30dfcff
akash Roy Choudhury <arc.ar602@gmail.com>
2021-06-15 11:47:02 +0530

    frontend layout complete

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -38,7 +38,8 @@ const ContactState = (props) => {
         type: "personal",
       },
     ],
-    current:null
+    current:null,
+    filtered:null
   };
 
   const [state, dispatch] = useReducer(contactReducer, initialState);
@@ -70,18 +71,28 @@ const ContactState = (props) => {
   }
 
   // filter contacts
+  const filterContacts= text =>{
+    dispatch({type:FILTER_CONTACTS, payload: text });
+  }
 
   // clear filter
+  const clearFilter= () =>{
+    dispatch({type:CLEAR_FILTER });
+  }
 
   return (
       <ContactContext.Provider value={{
           contacts:state.contacts,
           current:state.current,
+          filtered:state.filtered,
           addContact,
           deleteContact,
           setCurrent,
           clearCurrent,
-          updateContact
+          updateContact,
+          filterContacts,
+          clearFilter
+
       }}>
       { props.children }
       </ContactContext.Provider>
