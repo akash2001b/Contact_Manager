f443914b68bb675b523c7e42419f00be60d035e7
akash Roy Choudhury <arc.ar602@gmail.com>
2021-06-17 00:42:54 +0530

    Prepare for deployment

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express=require('express');
 const morgan = require("morgan");
 const connectDB = require('./config/db');
 const conectDB=require('./config/db') 
+const path=require('path');
 
 
 const app=express();
@@ -11,6 +12,17 @@ connectDB();
 
 // init middleware
 app.use(express.json({ extended: false }));
+
+
+// server static assests in production
+if(process.env.NODE_ENV==='production'){
+    // set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    })
+}
 
 const PORT=process.env.PORT || 5000;
 
