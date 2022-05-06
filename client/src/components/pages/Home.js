5fc277c103e854489d8614a783dbba4ff47f33c1
pinkman7009 <soumik.chaudhuri2000@gmail.com>
2022-05-06 20:10:16 +0530

    UI Changes made

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,29 +1,42 @@
-import React, { useContext, useEffect } from 'react';
-import Contacts from '../contacts/Contacts';
-import ContactForm from '../contacts/ContactForm';
-import ContactFilter from '../contacts/ContactFilter';
-import AuthContext from '../../context/auth/authContext';
+import React, { useContext, useEffect, useState } from "react";
+import Contacts from "../contacts/Contacts";
+import ContactForm from "../contacts/ContactForm";
+import ContactFilter from "../contacts/ContactFilter";
+import AuthContext from "../../context/auth/authContext";
 
 const Home = () => {
-    const authContext=useContext(AuthContext);
+  const authContext = useContext(AuthContext);
+  const [tab, setTab] = useState(0);
 
-    useEffect(() => {
-        authContext.loadUser();
-        //eslint-disable-next-line
-    }, [])
+  useEffect(() => {
+    authContext.loadUser();
+    //eslint-disable-next-line
+  }, []);
 
-    return (
-        <div className='grid-2'>
-            <div>
-                <ContactForm/>
-            </div>
-            <div>
-                <ContactFilter/>
-                <Contacts/>
-            </div>
-        </div>
-    )
-}
-
+  return (
+    <>
+      <div className="tab-group">
+        <button className="tab-button" onClick={() => setTab(0)}>
+          Contact Form
+        </button>
+        <button className="tab-button" onClick={() => setTab(1)}>
+          Contacts
+        </button>
+      </div>
+      <div className="grid-2">
+        {tab === 0 ? (
+          <div>
+            <ContactForm />
+          </div>
+        ) : (
+          <div>
+            <ContactFilter />
+            <Contacts />
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
 
 export default Home;
